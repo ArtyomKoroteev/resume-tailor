488063693fcceb2a865152b4fc0b2175364313ba
akorotieiev <akorotieiev@ascendix.com>
2026-05-22 18:53:19 +0200

    fixed formatter

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,15 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
+import heroImg from './assets/hero.png';
+import './App.css';
+import { Camera } from 'lucide-react';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-    <div><h1>Hello World</h1></div>
+      <div>
+        <h1>
+          Hello World
+          <Camera size={48} color="red" strokeWidth={1} />
+        </h1>
+      </div>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
