f49a5bc838636827ada43b06af3295c0c93095f5
akorotieiev <akorotieiev@ascendix.com>
2026-06-08 20:36:06 +0200

    added basic editor

---
+++ apps/web/src/features/preview/preview.tsx
@@ -0,0 +1,29 @@
+import { useResumeStore } from "../../stores";
+import showdown from "showdown";
+import { type AppearanceSettings } from "../../pages/Editor";
+
+interface PreviewProps {
+  appearanceSettings: AppearanceSettings;
+}
+export const Preview: React.FC<PreviewProps> = ({ appearanceSettings }) => {
+  const markdown = useResumeStore((state) => state.markdown);
+  const converter = new showdown.Converter();
+  const html = converter.makeHtml(markdown);
+  return (
+    <div className="wrapper w-9/12 bg-gray-100 p-3 overflow-y-auto max-h-screen">
+      <div
+        className="w-[768px] min-h-[1024px] bg-white m-auto wysiwyg-content"
+        dangerouslySetInnerHTML={{ __html: html }}
+        style={
+          {
+            "--font-size": `${appearanceSettings.fontSize}px`,
+            "--line-height": appearanceSettings.lineHeight,
+            "--text-color": appearanceSettings.textColor,
+            "--heading-color": appearanceSettings.headingColor,
+            "--page-padding": `${appearanceSettings.pagePadding}px`,
+          } as React.CSSProperties
+        }
+      />
+    </div>
+  );
+};
--- apps/web/src/layouts/main-layout.tsx
+++ apps/web/src/layouts/main-layout.tsx
@@ -1,15 +1,13 @@
 import { Outlet } from "react-router";
 import { Header } from "../shared/ui/header";
-import { Footer } from "../shared/ui/footer";
 
 export default function MainLayout() {
   return (
     <div className="flex min-h-screen flex-col">
-      <Header title="Preview" rightContent={<div>Right content</div>} />
-      <main className="flex-1 flex items-center justify-center">
+      <Header title="Preview" />
+      <main className="flex-1 flex">
         <Outlet />
       </main>
-      <Footer />
     </div>
   );
 }
